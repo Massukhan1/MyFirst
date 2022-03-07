83be7066b17a4a80254b3fb5cbc3939617c55442
Masihuddin <Masihuddin@LAPTOP-8SPHD86J>
2022-03-08 01:04:57 +0530

    access commit

---
+++ ABC/src/coding/Call.java
@@ -0,0 +1,10 @@
+package coding;
+
+public class Call {
+
+	public static void main(String[] args) {
+		System.out.println("&&&&&*******************8");
+		System.out.println("This is my first github repository");
+	}
+
+}
